3e32306112a942b371127e69a2ac68b3b2e2b903
Ruben Tan <soggie@gmail.com>
2021-06-28 00:22:22 +0800

    Updated instagram section

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -1,4 +1,7 @@
-fetch('https://www.instagram.com/sashaxuan/?__a=1')
+fetch('https://www.instagram.com/sashaxuan/?__a=1', {
+	// mode: 'no-cors',
+	redirect: 'follow',
+})
 	// .then(res => res.json())
 	.then(console.log)
 	.catch(console.log)
